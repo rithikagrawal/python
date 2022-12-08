a616af89b1a4f7117fc3b0fad913c8a7a4f77701
Rithik Agrawal <38203619+rithikagrawal@users.noreply.github.com>
2022-12-08 23:57:22 +0530

    Input Practice Code with typecasting.

---
+++ Input_Practice.py
@@ -0,0 +1,41 @@
+print("Hello World!!!")
+
+# Write a program to add two number
+
+a = int(input("Enter 1st Number: "))
+b = int(input("Enter 2nd Number: "))
+
+print("The sum of two numbers is:", a+b)
+
+# Write a python program to find a remainder when a number is divided by 7
+
+a = int(input("Enter a Number: "))
+
+print("The remainder of", a, "when divided by 7 is:", a % 7)
+
+# Check the type of the variable assigned using input function
+
+a = input("Enter: ")
+print(type(a))
+
+# Use comparison opertors to find out whether the given numbers a is greater than  or not take a=34 and b=80
+
+a = 34
+b = 80
+print(a > b)
+print(a < b)
+print(b > a)
+
+# Write a python program to find average of two numbers entered by the user
+
+a = int(input("Enter first Number: "))
+b = int(input("Enter second Number: "))
+
+print("The average of", a, "&", b, "is", (a+b)/2)
+
+# Write a python program to find the square of a number entered by the User
+
+a = int(input("Enter a Number: "))
+
+print("The square of", a, "is", a*a)  # Pure Logic
+print("The square of", a, "is", a**2)  # Using the operators provided by python
