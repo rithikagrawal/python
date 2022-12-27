ae3cc753a369fd7da8ecae5b51510f565ca97e2f
Rithik Agrawal <38203619+rithikagrawal@users.noreply.github.com>
2022-12-27 15:28:50 +0530

    Tuples Practice and Methods

---
+++ Tuples.py
@@ -0,0 +1,13 @@
+myTuple = (2, 4, 6)
+print(myTuple)
+# myTuple[0] = 9 # This will throw an error
+print(type(myTuple))
+
+# To create a tuple we need to put comma after a single item else it is treated as individual item
+# Example: myTuple(1,)
+
+# Methods
+
+tuple1 = (1, 7, 1, 2, 1)
+print(tuple1.count(1))
+print(tuple1.index(1))
